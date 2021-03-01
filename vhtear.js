dfaa7ad9e110cac92178849fee242ff4d86ab84e
ranggazs <74724682+ranggazs@users.noreply.github.com>
2021-03-01 08:12:35 +0800

    Update vhtear.js

--- vhtear.js
+++ vhtear.js
@@ -88,7 +88,7 @@ async function SBVHtear() {
 		const Qimage = type === 'extendedTextMessage' && sakura.includes('imageMessage')
 		let txt = (type === 'conversation' && m.message.conversation.startsWith(setkey)) ? m.message.conversation : (type == 'zzzimageMessage') && m.message.imageMessage.caption.startsWith(setkey) ? m.message.imageMessage.caption : (type == 'zzzvideoMessage') && m.message.videoMessage.caption.startsWith(setkey) ? m.message.videoMessage.caption : (type == 'extendedTextMessage') && m.message.extendedTextMessage.text.startsWith(setkey) ? m.message.extendedTextMessage.text : ''
 		if (m.key.fromMe) {
-			if (txt == "hi") {
+			if (txt == ":v") {
 				VHsendMessage(VH, to, "Hi sayang!!!")
 			} else if (txt == "me") {
 				VHsendMention(VH, to, "Hey @VHtears" + VH.user.jid, [VH.user.jid])
